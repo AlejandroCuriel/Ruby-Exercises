4e4c4bc554712efe77c5ac0d7945eb3804210e76
Alejandro Curiel <alex_curielg@hotmail.com>
2021-08-28 12:29:23 -0500

    Greeting exercise finished

---
+++ greets.rb
@@ -0,0 +1,5 @@
+# Write a program in the console that asks the user your name and
+# print "hellow" followed of name and exclamation mark
+print "What is your name?: "
+name = gets.chomp 
+puts "Hellow #{name}!"
